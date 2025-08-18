52707de063783ab080e022ad1ec59ea56a873634
Amruth <101964862+Amruth-B@users.noreply.github.com>
2025-08-18 13:58:11 +0530

    Add files via upload

---
+++ terraform-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # exit immediately if a command fails
+
+# Add HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add official HashiCorp Linux repo
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com \
+$(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update and install Terraform
+sudo apt update -y
+sudo apt install -y terraform
