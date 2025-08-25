4f4706140250bc8cdbc5cb7a2fdde3862d8787b0
Amruth <101964862+Amruth-B@users.noreply.github.com>
2025-08-25 22:51:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,4 +5,24 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Ansible server"
   }
+
+}
+resource "aws_instance" "master" {
+  ami           = "ami-0360c520857e3138f" # change it
+  instance_type = "t3.small"
+  security_groups = ["alltraffic-sg"] # change it
+
+  tags = {
+    Name = "Jenkins-master"
+  }
+}
+
+resource "aws_instance" "node" {
+  ami           = "ami-0360c520857e3138f" # change it
+  instance_type = "t3.small"
+  security_groups = ["alltraffic-sg"] # change it
+
+  tags = {
+    Name = "Application-Node"
+  }
 }
